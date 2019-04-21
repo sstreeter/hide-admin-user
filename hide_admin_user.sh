c415e571a00997ec369319c9f1004eb4862ddda3
Spencer Streeter <streeter@biology.utah.edu>
2019-04-20 22:39:01 -0600

    <# Test Commit v1 #>

--- hide_admin_user.sh
+++ hide_admin_user.sh
@@ -429,7 +429,7 @@ valid="$( validate_user $username )"
 echo -e "Valid User - $valid"
 if [[ $valid != "Yes" ]]; then echo -e "Invalid Username. Please only use an \"Account name\"\nof a user with administrative privileges!"; exit; fi
 
-# I am trying to connect to github so that I can save commits
+# Testing 123
 # 
 # # global vars
 # offset=100;
